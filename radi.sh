f8985f633447c237fbac93046c7b03d6db8f1a86
Muppetpants <jason.gray@student.sans.edu>
2024-04-08 16:54:19 -0400

    enabled legacy camera

--- radi.sh
+++ radi.sh
@@ -58,7 +58,7 @@ function installMotionEye() {
     if [ $distro -ne 1 ]
      then echo -e "\n  Bullseye Not Detected - Please flash bullseye and retry  \n"; exit
     fi
-
+    raspi-config nonint do_legacy 0
     apt update -y
     apt install python3-dev libcurl4-openssl-dev libssl-dev python3-pip
     pip3 install https://github.com/motioneye-project/motioneye/archive/dev.tar.gz
--- test.sh
+++
@@ -1,6 +0,0 @@
-check_distro() {
-    distro=$(cat /etc/os-release | grep -i -c "PRETTY_NAME=bullseye") # distro check
-    if [ $distro -ne 1 ]
-     then echo -e "\n  Bullseye Not Detected - Please flash bullseye and retry  \n"; exit
-    fi
-}
